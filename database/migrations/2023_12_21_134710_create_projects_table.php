34c30310dda4cee6ea5751f2059b9accdfd8bf1a
angelo friello <angelofriello01@gmail.com>
2024-01-09 14:46:15 +0100

    add migration type

--- database/migrations/2023_12_21_134710_create_projects_table.php
+++ database/migrations/2023_12_21_134710_create_projects_table.php
@@ -12,6 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('type-id')->nullable()->after('id');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null')->onUpdate('cascade');
+
             $table->id();
             $table->timestamps();
             $table->string('type');
@@ -27,6 +30,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::table('projects', function (Blueprint $table)
+        {
+            $table->dropForeign(['type_id']);
+            
+            $table->dropColumn('type_id');
+        });
     }
 };
